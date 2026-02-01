5648e9f13eae9e42f6374f629633f282260cf0a4
Andrei Volt <andrei@avolt.net>
2026-02-01 22:43:47 +0100

    add nix flake for reproducible builds and dev environment

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    maze-utils = {
+      url = "github:ajayyy/maze-utils";
+      flake = false;
+    };
+    locales = {
+      url = "github:ajayyy/ExtensionTranslations";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, maze-utils, locales }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        nodejs = pkgs.nodejs_22;
+
+        mkDeArrow = browser: pkgs.buildNpmPackage {
+          pname = "dearrow-${browser}";
+          version = "0.0.0-dev";
+          src = self;
+          inherit nodejs;
+
+          npmDepsHash = "sha256-50UVWOKWPHJeSsyUwi7iCcH1wYyLNdDGUoVZhGP5Ws8=";
+          makeCacheWritable = true;
+
+          env.CHROMEDRIVER_SKIP_DOWNLOAD = "true";
+          npmFlags = [ "--ignore-scripts" ];
+
+          postPatch = ''
+            cp -r ${maze-utils}/ maze-utils
+            cp -r ${locales}/ public/_locales
+          '';
+
+          buildPhase = ''
+            runHook preBuild
+            cp config.json.example config.json
+            npm run build:${browser}
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            cp -r dist $out
+            runHook postInstall
+          '';
+
+          dontNpmInstall = true;
+        };
+      in
+      {
+        packages = {
+          default = mkDeArrow "chrome";
+          chrome = mkDeArrow "chrome";
+          firefox = mkDeArrow "firefox";
+          safari = mkDeArrow "safari";
+          edge = mkDeArrow "edge";
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            nodejs
+            pkgs.web-ext
+          ];
+        };
+      }
+    );
+}
